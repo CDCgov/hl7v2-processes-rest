5a45fd828a3896f5844ffceb20dc48ce26cc5d9c
Neelima Kandukuri <dtx0@cdc.gov>
2024-08-27 11:41:18 -0400

    removed profile files

--- hl7v2-redactor/src/main/kotlin/cdc/gov/controllers/RedactorController.kt
+++ hl7v2-redactor/src/main/kotlin/cdc/gov/controllers/RedactorController.kt
@@ -16,17 +16,13 @@ import java.io.FileNotFoundException
 class RedactorController {
 
     @Post("/redactor", consumes = [MediaType.TEXT_PLAIN], produces = [MediaType.APPLICATION_JSON])  // Endpoint URL
-    fun redactMessage(@QueryValue dataStreamId: String?, @Body content: String,
+    fun redactMessage( @Body content: String,
                       request: HttpRequest<Any>): HttpResponse<Any> {
-        if (dataStreamId.isNullOrEmpty()) {
-            return HttpResponse
-                .badRequest("Error: Data Stream ID must be specified as a query parameter 'dataStreamId'.")
-        }
+
         var responseContent =""
 
          try {
-             val configFileName = getConfigFileName(dataStreamId)
-             val report = getRedactedReport(content ?: "", configFileName)
+             val report = getRedactedReport(content ?: "")
              if (report != null) {
                  responseContent = "{ \"report\": \"${report}\" }"
              }
@@ -40,21 +36,10 @@ class RedactorController {
 
     }
 
-    private fun getConfigFileName(dataStreamId: String ) : String {
-        val fileSuffix = "-config.txt"
-        val dataStreamName = dataStreamId.uppercase().trim()
-        // return without the '/' in front of the name for use in metadata
-        return if (this::class.java.getResource("/profiles/$dataStreamName$fileSuffix") != null) {
-            "$dataStreamName$fileSuffix"
-        } else {
-            // Use default (ELR) config if nothing else exists
-            "DEFAULT$fileSuffix"
-        }
-    }
-
-    fun getRedactedReport(msg: String, configFileName: String): Tuple2<String, List<RedactInfo>>? {
+    
+    fun getRedactedReport(msg: String): Tuple2<String, List<RedactInfo>>? {
         val dIdentifier = DeIdentifier()
-        val configFile = "/profiles/$configFileName"
+        val configFile = "/profiles/DEFAULT-config.txt"
         val rules = if (this::class.java.getResource(configFile) != null) {
             this::class.java.getResource(configFile)!!.readText().lines()
         } else {
